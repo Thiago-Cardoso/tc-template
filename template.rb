a729d1e642d7ba6c559d7a2ecc27bac0decea397
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2022-02-24 21:40:03 -0300

    feat: method constructor and dataset

--- template.rb
+++ template.rb
@@ -1,5 +1,26 @@
 class Template
-  
-end
+    attr_accessor :sentence
 
+    def initialize(sentence = nil)
+      @sentence = sentence
+    end
+
+    def clouses_data
+      clouses = [
+        { "id": 1, "text": 'The quick brown fox' },
+        { "id": 2, "text": 'jumps over the lazy dog' },
+        { "id": 3, "text": 'And dies' },
+        { "id": 4, "text": 'The white horse is white' }
+      ]
+      clouses
+    end
+
+    def sections_data
+      sections = [
+        { "id": 1, "clauses_ids": [1, 2] }
+      ]
+      sections
+    end
+
+  end
 
